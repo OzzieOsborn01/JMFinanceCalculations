f87b3f9591e9642ca3aa7b62d36829b45f429003
OzzieOsborn <andrewso.sax@gmail.com>
2025-09-05 14:56:19 -0400

    Establishing environment and initial dependencies

---
+++ processing/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("java")
+}
+
+group = "org.finance.calcs.processing"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // This dependency is exported to consumers, that is to say found on their compile classpath.
+    implementation(libs.commons.math3)
+
+    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+    implementation(libs.guava)
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation(libs.hamcrest)
+
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    testCompileOnly(libs.lombok)
+    testAnnotationProcessor(libs.lombok)
+
+    implementation(libs.mapstruct)
+    annotationProcessor(libs.mapstruct.processor)
+
+    implementation(libs.commons.lang3)
+
+    implementation(libs.jackson.databind)
+    annotationProcessor(libs.jackson.annotations)
+    implementation(libs.jackson.datatype.jsr310)
+    implementation(libs.jackson.datatype.jdk8)
+    implementation(project(":core"))
+    testImplementation(testFixtures(project(":core")))
+}
+
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use JUnit Jupiter test framework
+            useJUnitJupiter("5.12.1")
+        }
+    }
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
